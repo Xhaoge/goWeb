dde88adf3a22dff3c624a3cf0adb03686d0e9375
xxx <1026737316@qq.com>
2019-12-03 13:42:37 +0800

    add service

---
+++ rr-marineford/service/base.go
@@ -0,0 +1,13 @@
+package service
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/globalsign/mgo/bson"
+	"rr-factory.gloryholiday.com/yuetu/golang-core/logger"
+	"rr-factory.gloryholiday.com/yuetu/marineford/repository/ppconfig"
+	repo "rr-factory.gloryholiday.com/yuetu/marineford/repository"
+	"rr-factory.gloryholiday.com/yuetu/marineford"
+	"rr-factory.gloryholiday.com/yuetu/marineford/utils"
+)
--- rr-marineford/utils/utils.go
+++ rr-marineford/utils/utils.go
@@ -2,4 +2,19 @@ package utils
 
 import (
 	"context"
+	"math/big"
+	"time"
+	Nrand "crypto/rand"
+
+	conrectx "rr-factory.gloryholiday.com/yuetu/golang-core/context"
+	"rr-factory.gloryholiday.com/yuetu/yuetu/golang-core/location"
+	pb "rr-factory.gloryholiday.com/yuetu/marineford/proto/marineford"
 )
+
+func NewSimpleUpdateResponse(id string) * pb.SimpleUpdateResponse {
+	return &pb.SimpleUpdateResponse{Id: id}
+}
+
+func NewFailedSimpleUpdateResponse(id string,msg string) *pb.SimpleUpdateResponse {
+	return &pb.SimpleUpdateResponse{Id:id, Code:pb.ResponseStatusCode_FAILED,ErrMsg:msg}
+}

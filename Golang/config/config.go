28ce520bde3849e57e40576da6a442b0d03827f0
nima <1026737316@qq.com>
2019-12-10 23:11:27 +0800

    add yaml

--- Golang/config/config.go
+++ Golang/config/config.go
@@ -7,3 +7,42 @@ import (
 func config(){
 	fmt.Println("config")
 }
+
+type Yaml struct {
+	Mysql struct {
+		User 	string `yaml:"user"`
+		Host	string `yaml:"host"`
+		Password string `yaml:"password"`
+		Port	string  `yaml:"port"`
+		Name	string  `yaml:"name"`
+	}
+	Cache struct {
+		Enable  bool `yaml:"enable"`
+		List	[]string `yaml:"list,flow"`
+	}
+}
+
+
+type Yaml1 struct {
+	SQLConf Mysql `yaml:"mysql"`
+	CacheConf Cache `yaml:"cache"`
+}
+
+
+type Yaml12 struct {
+	Mysql 	`yaml:"mysql,inline"`
+	Cache 	`yaml:"cache,inline"`
+}
+
+type Mysql struct {
+	User 	string  	`yaml:"user"`
+	Hose    string 		`yaml:"host"`
+	Password  string 	`yaml:"password"`
+	Port 	string 		`yaml:"port"`
+	Name 	string  	`yaml:"name"`
+}
+
+type Cache struct {
+	Enable 	bool 	`yaml:"enable"`
+	List 	[]string `yaml:"list,flow"`
+}
--- Golang/main.go
+++ Golang/main.go
@@ -2,9 +2,23 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"Pro_golang/Golang/config"
+	yaml "gopkg.in/yaml.v2"
 )
 
 
+https://blog.csdn.net/kenkao/article/details/85071404
+
 func main(){
 	fmt.Println("hello world.......")
+	conf := new(config.Yaml)
+	yamlFile, err := ioutil.ReadFile("../case.yaml")
+	log.Println("yamlFile:",yamlFile)
+	if err != nil {
+		log.Printf("%v:",err)
+	}
+	log.Println("conf:",conf)
+	fmt.Println(yamlFile)
 }
+

a4a238f869cc794f5d40b6f944afaa7029f689e6
xxx <1026737316@qq.com>
2019-12-17 09:36:05 +0800

    add

--- Golang/main.go
+++ Golang/main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"fmt"
 	"Pro_golang/Golang/config"
-	"Pro_golang/Golang/TestCase/SearchCase"
-	_"Pro_golang/Golang/utils"
+	//"Pro_golang/Golang/TestCase/SearchCase"
+	"Pro_golang/Golang/utils"
 )
 
 func main(){
 	fmt.Println("hello world.......")
 	fmt.Printf("CaseConfig main:%+v \n",config.CaseConfig)
 	fmt.Printf("BaseConfig main: %+v \n",config.BaseConfig)
-	searchCase.RunCaseProcess()
+	//searchCase.RunCaseProcess()
+	utils.Utils()
 }
 
--- Golang/utils/utils.go
+++ Golang/utils/utils.go
@@ -5,28 +5,34 @@ import (
 	"fmt"
 	"Pro_golang/Golang/config"
 	"reflect"
-	"Pro_golang/Golang/TestCase/Search"
+	//"Pro_golang/Golang/TestCase/Search"
 	//"Pro_golang/Golang/TestCase/SearchCase"
 )
 
-func utils() {
+func Utils() {
 	fmt.Println("this is utils package")
 }
 
-var regStruct map[string]interface{}
+
 
 func init(){
 	fmt.Println("this is utils package")
+	var regStruct map[string]interface{}
+	regStruct = make(map[string]interface{})
+	fmt.Println("regStruct:",regStruct)
+	
 	for _,c := range config.CaseConfig.Cases {
 		fmt.Println(c.Casename)
 		str := c.Casename
+	
+		fmt.Println("str:",str)
 		// str2 := strconv.Itoa(str1)
 		// str := string(str2)
 		if regStruct[str] != nil {
-			t := reflect.ValueOf(regStruct[str]).Type()
-			v := reflect.New(t).Elem()
-			fmt.Println("t,v:",t,v)
-			v.RunCaseProcess()
+		 	t := reflect.ValueOf(regStruct[str]).Type()
+		 	v := reflect.New(t).Elem()
+		 	fmt.Println("t,v:",t,v)
+		// 	v.RunCaseProcess()
 
 		//runCase := new(c.Casename{})
 	 	}

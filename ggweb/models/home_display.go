f77b243d46ef9357189c05eb7df0466be5db4710
nima <1026737316@qq.com>
2020-02-05 14:06:54 +0800

    add showPage

--- ggweb/models/home_display.go
+++ ggweb/models/home_display.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"html/template"
+	"github.com/astaxie/beego"
 	"strconv"
 	"strings"
 )
@@ -81,4 +82,23 @@ func createTagsLinks(tags string) []TagLink {
 	return tagLink
 }
 
-// 查询
+// 查询文章和修改方法
+func ConfigHomeFooterPageCode(page int) HomeFooterPageCode{
+	pageCode := HomeFooterPageCode{}
+	// 查询出总的条数
+	num := GetArticleRowsNum()
+	// 从配置文件中读取每页显示的条数
+	pageRow, _ := beego.AppConfig.Int("articleListPageNum")
+	//计算出总页数
+	allPageNum := (num-1)/pageRow +1
+	pageCode.ShowPage = fmt.Sprintf("%d/%d",page,allPageNum)
+	// 当前页数大于总页数，那么下一页的按钮不能点击
+	if page >= allPageNum {
+		pageCode.HasNext = false
+	} else {
+		pageCode.HasNext = true
+	}
+	pageCode.PreLink = "/?page="+ strconv.Itoa(page-1)
+	pageCode.NextLink = "/?page=" + strconv.Itoa(page+1)
+	return pageCode
+}
